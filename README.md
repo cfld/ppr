1b5088b96a6b795ceaec855cda19008ff53f7426
bkj <bkj.322@gmail.com>
2020-09-04 15:42:46 -0400

    docs

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # omp_ref_workloads
+
+## Usage
+
+```
+mkdir bin
+make
+./bin/ppr
+```
+
+## Changing datasets
+
+`pack-mm.py` is a Python script that converts a Matrix Market graph to binary, which is read by `ppr`.
--- pack-mm.py
+++ pack-mm.py
@@ -2,7 +2,9 @@
 
 from scipy.io import mmread
 
-adj = mmread('data/jhu.mtx').tocsr()
+inpath  = 'data/jhu.mtx'
+outpath = 'data/jhu.bin'
+adj     = mmread(inpath).tocsr()
 
 n_nodes = adj.shape[0]
 n_edges = adj.nnz
@@ -11,8 +13,7 @@ packed_data = np.hstack([
     np.int64(n_nodes),
     np.int64(n_edges),
     adj.indptr.astype(np.int64),
-    adj.indices.astype(np.int64),
-    adj.data.astype(np.int64)
+    adj.indices.astype(np.int64)
 ])
 
-_  = open('jhu.bin', 'wb').write(bytearray(packed_data))
+_  = open(outpath, 'wb').write(bytearray(packed_data))
